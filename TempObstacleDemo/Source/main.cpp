8157a4ded23ac0a62186a661795f7579c7173ebe
ZLSYMDJ <410878589@qq.com>
2019-04-16 10:30:08 +0800

    Add files via upload

---
+++ TempObstacleDemo/Source/main.cpp
@@ -0,0 +1,719 @@
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+#include "DetourNavMesh.h"
+#include "DetourNavMeshQuery.h"
+#include "DetourNavMeshBuilder.h"
+#include "DetourCommon.h"
+#include "DetourTileCache.h"
+#include "DetourTileCacheBuilder.h"
+#include "InputGeom.h"
+#include "fastlz.h"
+
+static const int TILECACHESET_MAGIC = 'T' << 24 | 'S' << 16 | 'E' << 8 | 'T'; //'TSET';
+static const int TILECACHESET_VERSION = 1;
+
+struct TileCacheSetHeader
+{
+	int magic;
+	int version;
+	int numTiles;
+	dtNavMeshParams meshParams;
+	dtTileCacheParams cacheParams;
+};
+
+struct TileCacheTileHeader
+{
+	dtCompressedTileRef tileRef;
+	int dataSize;
+};
+
+enum SamplePolyAreas
+{
+	SAMPLE_POLYAREA_GROUND,
+	SAMPLE_POLYAREA_WATER,
+	SAMPLE_POLYAREA_ROAD,
+	SAMPLE_POLYAREA_DOOR,
+	SAMPLE_POLYAREA_GRASS,
+	SAMPLE_POLYAREA_JUMP,
+};
+enum SamplePolyFlags
+{
+	SAMPLE_POLYFLAGS_WALK = 0x01,		// Ability to walk (ground, grass, road)
+	SAMPLE_POLYFLAGS_SWIM = 0x02,		// Ability to swim (water).
+	SAMPLE_POLYFLAGS_DOOR = 0x04,		// Ability to move through doors.
+	SAMPLE_POLYFLAGS_JUMP = 0x08,		// Ability to jump.
+	SAMPLE_POLYFLAGS_DISABLED = 0x10,		// Disabled polygon
+	SAMPLE_POLYFLAGS_ALL = 0xffff	// All abilities.
+};
+
+template<class T> inline T rcMin(T a, T b) { return a < b ? a : b; }
+
+template<class T> inline T rcMax(T a, T b) { return a > b ? a : b; }
+
+inline bool inRange(const float* v1, const float* v2, const float r, const float h)
+{
+	const float dx = v2[0] - v1[0];
+	const float dy = v2[1] - v1[1];
+	const float dz = v2[2] - v1[2];
+	return (dx*dx + dz*dz) < r*r && fabsf(dy) < h;
+}
+
+static bool getSteerTarget(dtNavMeshQuery* navQuery, const float* startPos, const float* endPos,
+	const float minTargetDist,
+	const dtPolyRef* path, const int pathSize,
+	float* steerPos, unsigned char& steerPosFlag, dtPolyRef& steerPosRef,
+	float* outPoints = 0, int* outPointCount = 0)
+{
+	// Find steer target.
+	static const int MAX_STEER_POINTS = 3;
+	float steerPath[MAX_STEER_POINTS * 3];
+	unsigned char steerPathFlags[MAX_STEER_POINTS];
+	dtPolyRef steerPathPolys[MAX_STEER_POINTS];
+	int nsteerPath = 0;
+	navQuery->findStraightPath(startPos, endPos, path, pathSize,
+		steerPath, steerPathFlags, steerPathPolys, &nsteerPath, MAX_STEER_POINTS);
+	if (!nsteerPath)
+		return false;
+
+	if (outPoints && outPointCount)
+	{
+		*outPointCount = nsteerPath;
+		for (int i = 0; i < nsteerPath; ++i)
+			dtVcopy(&outPoints[i * 3], &steerPath[i * 3]);
+	}
+
+
+	// Find vertex far enough to steer to.
+	int ns = 0;
+	while (ns < nsteerPath)
+	{
+		// Stop at Off-Mesh link or when point is further than slop away.
+		if ((steerPathFlags[ns] & DT_STRAIGHTPATH_OFFMESH_CONNECTION) ||
+			!inRange(&steerPath[ns * 3], startPos, minTargetDist, 1000.0f))
+			break;
+		ns++;
+	}
+	// Failed to find good point to steer to.
+	if (ns >= nsteerPath)
+		return false;
+
+	dtVcopy(steerPos, &steerPath[ns * 3]);
+	steerPos[1] = startPos[1];
+	steerPosFlag = steerPathFlags[ns];
+	steerPosRef = steerPathPolys[ns];
+
+	return true;
+}
+
+static int fixupCorridor(dtPolyRef* path, const int npath, const int maxPath,
+	const dtPolyRef* visited, const int nvisited)
+{
+	int furthestPath = -1;
+	int furthestVisited = -1;
+
+	// Find furthest common polygon.
+	for (int i = npath - 1; i >= 0; --i)
+	{
+		bool found = false;
+		for (int j = nvisited - 1; j >= 0; --j)
+		{
+			if (path[i] == visited[j])
+			{
+				furthestPath = i;
+				furthestVisited = j;
+				found = true;
+			}
+		}
+		if (found)
+			break;
+	}
+
+	// If no intersection found just return current path. 
+	if (furthestPath == -1 || furthestVisited == -1)
+		return npath;
+
+	// Concatenate paths.	
+
+	// Adjust beginning of the buffer to include the visited.
+	const int req = nvisited - furthestVisited;
+	const int orig = rcMin(furthestPath + 1, npath);
+	int size = rcMax(0, npath - orig);
+	if (req + size > maxPath)
+		size = maxPath - req;
+	if (size)
+		memmove(path + req, path + orig, size * sizeof(dtPolyRef));
+
+	// Store visited
+	for (int i = 0; i < req; ++i)
+		path[i] = visited[(nvisited - 1) - i];
+
+	return req + size;
+}
+
+// This function checks if the path has a small U-turn, that is,
+// a polygon further in the path is adjacent to the first polygon
+// in the path. If that happens, a shortcut is taken.
+// This can happen if the target (T) location is at tile boundary,
+// and we're (S) approaching it parallel to the tile edge.
+// The choice at the vertex can be arbitrary, 
+//  +---+---+
+//  |:::|:::|
+//  +-S-+-T-+
+//  |:::|   | <-- the step can end up in here, resulting U-turn path.
+//  +---+---+
+static int fixupShortcuts(dtPolyRef* path, int npath, dtNavMeshQuery* navQuery)
+{
+	if (npath < 3)
+		return npath;
+
+	// Get connected polygons
+	static const int maxNeis = 16;
+	dtPolyRef neis[maxNeis];
+	int nneis = 0;
+
+	const dtMeshTile* tile = 0;
+	const dtPoly* poly = 0;
+	if (dtStatusFailed(navQuery->getAttachedNavMesh()->getTileAndPolyByRef(path[0], &tile, &poly)))
+		return npath;
+
+	for (unsigned int k = poly->firstLink; k != DT_NULL_LINK; k = tile->links[k].next)
+	{
+		const dtLink* link = &tile->links[k];
+		if (link->ref != 0)
+		{
+			if (nneis < maxNeis)
+				neis[nneis++] = link->ref;
+		}
+	}
+
+	// If any of the neighbour polygons is within the next few polygons
+	// in the path, short cut to that polygon directly.
+	static const int maxLookAhead = 6;
+	int cut = 0;
+	for (int i = dtMin(maxLookAhead, npath) - 1; i > 1 && cut == 0; i--) {
+		for (int j = 0; j < nneis; j++)
+		{
+			if (path[i] == neis[j]) {
+				cut = i;
+				break;
+			}
+		}
+	}
+	if (cut > 1)
+	{
+		int offset = cut - 1;
+		npath -= offset;
+		for (int i = 1; i < npath; i++)
+			path[i] = path[i + offset];
+	}
+
+	return npath;
+}
+
+struct FastLZCompressor : public dtTileCacheCompressor
+{
+	virtual int maxCompressedSize(const int bufferSize)
+	{
+		return (int)(bufferSize* 1.05f);
+	}
+
+	virtual dtStatus compress(const unsigned char* buffer, const int bufferSize,
+		unsigned char* compressed, const int /*maxCompressedSize*/, int* compressedSize)
+	{
+		*compressedSize = fastlz_compress((const void *const)buffer, bufferSize, compressed);
+		return DT_SUCCESS;
+	}
+
+	virtual dtStatus decompress(const unsigned char* compressed, const int compressedSize,
+		unsigned char* buffer, const int maxBufferSize, int* bufferSize)
+	{
+		*bufferSize = fastlz_decompress(compressed, compressedSize, buffer, maxBufferSize);
+		return *bufferSize < 0 ? DT_FAILURE : DT_SUCCESS;
+	}
+};
+
+struct LinearAllocator : public dtTileCacheAlloc
+{
+	unsigned char* buffer;
+	size_t capacity;
+	size_t top;
+	size_t high;
+
+	LinearAllocator(const size_t cap) : buffer(0), capacity(0), top(0), high(0)
+	{
+		resize(cap);
+	}
+
+	~LinearAllocator()
+	{
+		dtFree(buffer);
+	}
+
+	void resize(const size_t cap)
+	{
+		if (buffer) dtFree(buffer);
+		buffer = (unsigned char*)dtAlloc(cap, DT_ALLOC_PERM);
+		capacity = cap;
+	}
+
+	virtual void reset()
+	{
+		high = dtMax(high, top);
+		top = 0;
+	}
+
+	virtual void* alloc(const size_t size)
+	{
+		if (!buffer)
+			return 0;
+		if (top + size > capacity)
+			return 0;
+		unsigned char* mem = &buffer[top];
+		top += size;
+		return mem;
+	}
+
+	virtual void free(void* /*ptr*/)
+	{
+		// Empty
+	}
+};
+
+struct MeshProcess : public dtTileCacheMeshProcess
+{
+	InputGeom* m_geom;
+
+	inline MeshProcess() : m_geom(0)
+	{
+	}
+
+	inline void init(InputGeom* geom)
+	{
+		m_geom = geom;
+	}
+
+	virtual void process(struct dtNavMeshCreateParams* params,
+		unsigned char* polyAreas, unsigned short* polyFlags)
+	{
+		// Update poly flags from areas.
+		for (int i = 0; i < params->polyCount; ++i)
+		{
+			if (polyAreas[i] == DT_TILECACHE_WALKABLE_AREA)
+				polyAreas[i] = SAMPLE_POLYAREA_GROUND;
+
+			if (polyAreas[i] == SAMPLE_POLYAREA_GROUND ||
+				polyAreas[i] == SAMPLE_POLYAREA_GRASS ||
+				polyAreas[i] == SAMPLE_POLYAREA_ROAD)
+			{
+				polyFlags[i] = SAMPLE_POLYFLAGS_WALK;
+			}
+			else if (polyAreas[i] == SAMPLE_POLYAREA_WATER)
+			{
+				polyFlags[i] = SAMPLE_POLYFLAGS_SWIM;
+			}
+			else if (polyAreas[i] == SAMPLE_POLYAREA_DOOR)
+			{
+				polyFlags[i] = SAMPLE_POLYFLAGS_WALK | SAMPLE_POLYFLAGS_DOOR;
+			}
+		}
+
+		// Pass in off-mesh connections.
+		if (m_geom)
+		{
+			params->offMeshConVerts = m_geom->getOffMeshConnectionVerts();
+			params->offMeshConRad = m_geom->getOffMeshConnectionRads();
+			params->offMeshConDir = m_geom->getOffMeshConnectionDirs();
+			params->offMeshConAreas = m_geom->getOffMeshConnectionAreas();
+			params->offMeshConFlags = m_geom->getOffMeshConnectionFlags();
+			params->offMeshConUserID = m_geom->getOffMeshConnectionId();
+			params->offMeshConCount = m_geom->getOffMeshConnectionCount();
+		}
+	}
+};
+
+class TesterTool
+{
+	dtNavMesh* m_navMesh;
+	dtNavMeshQuery* m_navQuery;
+	dtQueryFilter m_filter;
+	float m_polyPickExt[3];
+
+	static const int MAX_POLYS = 256;
+	static const int MAX_SMOOTH = 2048;
+
+	float m_spos[3];
+	float m_epos[3];
+	dtPolyRef m_startRef;
+	dtPolyRef m_endRef;
+	dtPolyRef m_polys[MAX_POLYS];
+	dtPolyRef m_parent[MAX_POLYS];
+	int m_npolys;
+	int m_nsmoothPath;
+	float m_smoothPath[MAX_SMOOTH * 3];
+
+///////////TmpObstacle///////////
+	struct LinearAllocator* m_talloc;
+	struct FastLZCompressor* m_tcomp;
+	struct MeshProcess* m_tmproc;
+
+	class dtTileCache* m_tileCache;
+
+public:
+	TesterTool();
+	void LoadMeshFile(const char* file_name);
+	void LoadNavMesh(const char* file_name);
+	void FindPath(float sp[3],float ep[3]);
+	void Update();
+	void AddObstacle(float *p);
+	void RemoveOneObstacle(int idx);
+	void RemoveAllObstacle();
+};
+
+TesterTool::TesterTool() :m_navMesh(0), m_navQuery(0)
+{
+	m_navQuery = dtAllocNavMeshQuery();
+
+	m_filter.setIncludeFlags(SAMPLE_POLYFLAGS_ALL ^ SAMPLE_POLYFLAGS_DISABLED);
+	m_filter.setExcludeFlags(0);
+
+	m_polyPickExt[0] = 2;
+	m_polyPickExt[1] = 4;
+	m_polyPickExt[2] = 2;
+
+	m_talloc = new LinearAllocator(32000);
+	m_tcomp = new FastLZCompressor;
+	m_tmproc = new MeshProcess;
+}
+
+void TesterTool::LoadMeshFile(const char* file_name){
+	FILE* fp = fopen(file_name, "rb");
+	if (!fp) return;
+
+	// Read header.
+	TileCacheSetHeader header;
+	size_t headerReadReturnCode = fread(&header, sizeof(TileCacheSetHeader), 1, fp);
+	if (headerReadReturnCode != 1)
+	{
+		// Error or early EOF
+		fclose(fp);
+		return;
+	}
+	if (header.magic != TILECACHESET_MAGIC)
+	{
+		fclose(fp);
+		return;
+	}
+	if (header.version != TILECACHESET_VERSION)
+	{
+		fclose(fp);
+		return;
+	}
+
+	m_navMesh = dtAllocNavMesh();
+	if (!m_navMesh)
+	{
+		fclose(fp);
+		return;
+	}
+	dtStatus status = m_navMesh->init(&header.meshParams);
+	if (dtStatusFailed(status))
+	{
+		fclose(fp);
+		return;
+	}
+
+	m_tileCache = dtAllocTileCache();
+	if (!m_tileCache)
+	{
+		fclose(fp);
+		return;
+	}
+	status = m_tileCache->init(&header.cacheParams, m_talloc, m_tcomp, m_tmproc);
+	if (dtStatusFailed(status))
+	{
+		fclose(fp);
+		return;
+	}
+
+	// Read tiles.
+	for (int i = 0; i < header.numTiles; ++i)
+	{
+		TileCacheTileHeader tileHeader;
+		size_t tileHeaderReadReturnCode = fread(&tileHeader, sizeof(tileHeader), 1, fp);
+		if (tileHeaderReadReturnCode != 1)
+		{
+			// Error or early EOF
+			fclose(fp);
+			return;
+		}
+		if (!tileHeader.tileRef || !tileHeader.dataSize)
+			break;
+
+		unsigned char* data = (unsigned char*)dtAlloc(tileHeader.dataSize, DT_ALLOC_PERM);
+		if (!data) break;
+		memset(data, 0, tileHeader.dataSize);
+		size_t tileDataReadReturnCode = fread(data, tileHeader.dataSize, 1, fp);
+		if (tileDataReadReturnCode != 1)
+		{
+			// Error or early EOF
+			dtFree(data);
+			fclose(fp);
+			return;
+		}
+
+		dtCompressedTileRef tile = 0;
+		dtStatus addTileStatus = m_tileCache->addTile(data, tileHeader.dataSize, DT_COMPRESSEDTILE_FREE_DATA, &tile);
+		if (dtStatusFailed(addTileStatus))
+		{
+			dtFree(data);
+		}
+
+		if (tile)
+			m_tileCache->buildNavMeshTile(tile, m_navMesh);
+	}
+
+	fclose(fp);
+}
+
+void TesterTool::LoadNavMesh(const char* file_name) {
+	if (!file_name) {
+		printf("Error File Name!\n");
+		return;
+	}
+
+	dtFreeNavMesh(m_navMesh);
+	dtFreeTileCache(m_tileCache);
+	LoadMeshFile(file_name);
+	m_navQuery->init(m_navMesh, 2048);
+}
+
+void TesterTool::FindPath(float sp[3], float ep[3]) {
+	for (int i = 0; i < 3; i++) {
+		m_spos[i] = sp[i];
+		m_epos[i] = ep[i];
+	}
+
+	m_navQuery->findNearestPoly(m_spos, m_polyPickExt, &m_filter, &m_startRef, 0);
+	m_navQuery->findNearestPoly(m_epos, m_polyPickExt, &m_filter, &m_endRef, 0);
+
+	m_navQuery->findPath(m_startRef, m_endRef, m_spos, m_epos, &m_filter, m_polys, &m_npolys, MAX_POLYS);
+	for (int i = 0; i < m_npolys; i++) {
+		printf("poly:%d ", m_polys[i]);
+	}
+	printf("\n");
+
+	m_nsmoothPath = 0;
+
+	if (m_npolys)
+	{
+		// Iterate over the path to find smooth path on the detail mesh surface.
+		dtPolyRef polys[MAX_POLYS];
+		memcpy(polys, m_polys, sizeof(dtPolyRef)*m_npolys);
+		int npolys = m_npolys;
+
+		float iterPos[3], targetPos[3];
+		m_navQuery->closestPointOnPoly(m_startRef, m_spos, iterPos, 0);
+		m_navQuery->closestPointOnPoly(polys[npolys - 1], m_epos, targetPos, 0);
+
+		static const float STEP_SIZE = 0.5f;
+		static const float SLOP = 0.01f;
+
+		m_nsmoothPath = 0;
+
+		dtVcopy(&m_smoothPath[m_nsmoothPath * 3], iterPos);
+		m_nsmoothPath++;
+
+		// Move towards target a small advancement at a time until target reached or
+		// when ran out of memory to store the path.
+		while (npolys && m_nsmoothPath < MAX_SMOOTH)
+		{
+			// Find location to steer towards.
+			float steerPos[3];
+			unsigned char steerPosFlag;
+			dtPolyRef steerPosRef;
+
+			if (!getSteerTarget(m_navQuery, iterPos, targetPos, SLOP,
+				polys, npolys, steerPos, steerPosFlag, steerPosRef))
+				break;
+
+			bool endOfPath = (steerPosFlag & DT_STRAIGHTPATH_END) ? true : false;
+			bool offMeshConnection = (steerPosFlag & DT_STRAIGHTPATH_OFFMESH_CONNECTION) ? true : false;
+
+			// Find movement delta.
+			float delta[3], len;
+			dtVsub(delta, steerPos, iterPos);
+			len = dtMathSqrtf(dtVdot(delta, delta));
+			// If the steer target is end of path or off-mesh link, do not move past the location.
+			if ((endOfPath || offMeshConnection) && len < STEP_SIZE)
+				len = 1;
+			else
+				len = STEP_SIZE / len;
+			float moveTgt[3];
+			dtVmad(moveTgt, iterPos, delta, len);
+
+			// Move
+			float result[3];
+			dtPolyRef visited[16];
+			int nvisited = 0;
+			m_navQuery->moveAlongSurface(polys[0], iterPos, moveTgt, &m_filter,
+				result, visited, &nvisited, 16);
+
+			npolys = fixupCorridor(polys, npolys, MAX_POLYS, visited, nvisited);
+			npolys = fixupShortcuts(polys, npolys, m_navQuery);
+
+			float h = 0;
+			m_navQuery->getPolyHeight(polys[0], result, &h);
+			result[1] = h;
+			dtVcopy(iterPos, result);
+
+			// Handle end of path and off-mesh links when close enough.
+			if (endOfPath && inRange(iterPos, steerPos, SLOP, 1.0f))
+			{
+				// Reached end of path.
+				dtVcopy(iterPos, targetPos);
+				if (m_nsmoothPath < MAX_SMOOTH)
+				{
+					dtVcopy(&m_smoothPath[m_nsmoothPath * 3], iterPos);
+					m_nsmoothPath++;
+				}
+				break;
+			}
+			else if (offMeshConnection && inRange(iterPos, steerPos, SLOP, 1.0f))
+			{
+				// Reached off-mesh connection.
+				float startPos[3], endPos[3];
+
+				// Advance the path up to and over the off-mesh connection.
+				dtPolyRef prevRef = 0, polyRef = polys[0];
+				int npos = 0;
+				while (npos < npolys && polyRef != steerPosRef)
+				{
+					prevRef = polyRef;
+					polyRef = polys[npos];
+					npos++;
+				}
+				for (int i = npos; i < npolys; ++i)
+					polys[i - npos] = polys[i];
+				npolys -= npos;
+
+				// Handle the connection.
+				dtStatus status = m_navMesh->getOffMeshConnectionPolyEndPoints(prevRef, polyRef, startPos, endPos);
+				if (dtStatusSucceed(status))
+				{
+					if (m_nsmoothPath < MAX_SMOOTH)
+					{
+						dtVcopy(&m_smoothPath[m_nsmoothPath * 3], startPos);
+						m_nsmoothPath++;
+						// Hack to make the dotted path not visible during off-mesh connection.
+						if (m_nsmoothPath & 1)
+						{
+							dtVcopy(&m_smoothPath[m_nsmoothPath * 3], startPos);
+							m_nsmoothPath++;
+						}
+					}
+					// Move position at the other side of the off-mesh link.
+					dtVcopy(iterPos, endPos);
+					float eh = 0.0f;
+					m_navQuery->getPolyHeight(polys[0], iterPos, &eh);
+					iterPos[1] = eh;
+				}
+			}
+
+			// Store results.
+			if (m_nsmoothPath < MAX_SMOOTH)
+			{
+				dtVcopy(&m_smoothPath[m_nsmoothPath * 3], iterPos);
+				m_nsmoothPath++;
+			}
+		}
+
+		///Test
+		for (int i = 0; i < m_nsmoothPath; i += 10) {
+			printf("path point[%.3f,%.3f,%.3f] - ", m_smoothPath[i * 3], m_smoothPath[i * 3 + 1], m_smoothPath[i * 3 + 2]);
+		}
+		printf("\n");
+	}
+}
+
+void TesterTool::Update() {
+	if (!m_navMesh)
+		return;
+	if (!m_tileCache)
+		return;
+
+	int update_times = 10;///不在帧中做update,这里直接饱和更新
+	while (update_times--) {
+		m_tileCache->update(0.1f, m_navMesh);
+	}
+}
+
+void TesterTool::AddObstacle(float *p) {
+	m_tileCache->addObstacle(p, 1.0f, 2.0f, 0);
+}
+
+void TesterTool::RemoveOneObstacle(int idx) {
+	if (!m_tileCache) {
+		return;
+	}
+
+	if (idx >= m_tileCache->getObstacleCount()) {
+		printf("Too Large Idx!\n");
+		return;
+	}
+
+	const dtTileCacheObstacle* ob = m_tileCache->getObstacle(idx);
+	if (ob->state == DT_OBSTACLE_EMPTY) {
+		return;
+	}
+
+	m_tileCache->removeObstacle(m_tileCache->getObstacleRef(ob));
+}
+
+void TesterTool::RemoveAllObstacle() {
+	if (!m_tileCache)
+		return;
+	for (int i = 0; i < m_tileCache->getObstacleCount(); ++i)
+	{
+		const dtTileCacheObstacle* ob = m_tileCache->getObstacle(i);
+		if (ob->state == DT_OBSTACLE_EMPTY) continue;
+		m_tileCache->removeObstacle(m_tileCache->getObstacleRef(ob));
+	}
+}
+
+int main() {
+	TesterTool tool;
+	tool.LoadNavMesh("all_tiles_tilecache.bin");
+	printf("hello nav!\n");
+	float sp[3], ep[3];
+	float obp[3];
+	char op_str[10];
+	while (scanf("%s", op_str)) {
+		if (0 == strcmp(op_str, "path")) {
+			printf("input two points:");
+			scanf("%f%f%f%f%f%f", &sp[0], &sp[1], &sp[2], &ep[0], &ep[1], &ep[2]);
+			tool.FindPath(sp, ep);
+		}
+		else if (0 == strcmp(op_str, "ob")) {
+			printf("inupt obstacle pos:");
+			scanf("%f%f%f", &obp[0], &obp[1], &obp[2]);
+			tool.AddObstacle(obp);
+			tool.Update();
+		}
+		else if (0 == strcmp(op_str, "rm")) {
+			printf("input obstracle idx:");
+			int idx;
+			scanf("%d", &idx);
+			tool.RemoveOneObstacle(idx);
+			tool.Update();
+		}
+		else if (0 == strcmp(op_str, "rmall")) {
+			tool.RemoveAllObstacle();
+			tool.Update();
+		}
+		else {
+			printf("Err op\n");
+		}
+	}
+	return 0;
+}
